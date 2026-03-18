3ca2ab7d9b94efe3965b87119f41f177e68bfab7
Your Name <you@example.com>
2026-03-18 20:21:32 +0530

    Wire up routing and connect all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import ContactSection from './components/ContactSection'
-import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <ContactSection />
-      <Footer />
-    </>
-  )
+    <Router>
+      <div className="flex min-h-screen flex-col bg-white">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<ContactSection />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
