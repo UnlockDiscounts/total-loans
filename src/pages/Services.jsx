600db912561b603f1bf38934a2a7cf1219efb184
SahityaNaik <sahityanaik03@gmail.com>
2026-03-18 14:06:37 +0530

    feat: implement home page and navbar component

---
+++ src/pages/Services.jsx
@@ -0,0 +1,9 @@
+function Services() {
+  return (
+    <div className="flex items-center justify-center min-h-[50vh]">
+      <h1 className="text-4xl font-bold">Services Page</h1>
+    </div>
+  );
+}
+
+export default Services;
