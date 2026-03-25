ebd9edf26c09dcdfca359ac143a4057e0f8c94fb
Your Name <you@example.com>
2026-03-26 01:45:12 +0530

    Untrack all files from repository

--- src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-function Contact() {
-  return (
-    <div className="flex items-center justify-center min-h-[50vh]">
-      <h1 className="text-4xl font-bold">Contact Page</h1>
-    </div>
-  );
-}
-
-export default Contact;
