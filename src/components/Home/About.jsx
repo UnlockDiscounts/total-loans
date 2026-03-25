afdf645ef3c4fcebc542f1cb6603a4e804e3d364
Your Name <you@example.com>
2026-03-26 01:08:57 +0530

    update 1

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,44 +1,17 @@
 import React from "react";
-import aboutImage1 from "../../assets/images/about-image1.png";
-import aboutImage2 from "../../assets/images/about-image2.png";
+import AboutDesign from "./AboutDesign";
 import shieldIcon from "../../assets/icons/shield.svg";
 import clockIcon from "../../assets/icons/clock.svg";
+import FeatureList from "./FeatureList";
+  
 
 function About() {
   return (
     <section className="w-full bg-white px-6 lg:px-0 lg:mt-[45px]">
       <div className="lg:max-w-[1314px] lg:min-h-[589px] lg:ml-[80px] grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center">
-        {/* Left Side: Layered Images */}
-        <div className="relative order-2 lg:order-1 flex justify-center lg:block lg:min-h-[589px]">
-          {/* Base Image */}
-          <div className="lg:absolute lg:mt-0 lg:ml-0 w-full max-w-[411px] lg:w-[411px] h-[395px] bg-gray-200 rounded-[15px] overflow-hidden shadow-[0px_8px_30px_0px_#00000040] z-10">
-            <img
-              src={aboutImage1}
-              alt="About Image 1"
-              className="w-full h-full object-cover"
-            />
-          </div>
-
-          {/* Overlap Image */}
-          <div className="lg:absolute lg:top-[134px] lg:left-[304px] w-full max-w-[305px] lg:w-[305px] h-[405px] rounded-[15px] shadow-[0px_8px_30px_0px_#00000040] z-20 overflow-hidden mt-8 lg:mt-0">
-            <img
-              src={aboutImage2}
-              alt="About Image 2"
-              className="w-full h-full object-cover scale-[1.15]"
-            />
-          </div>
-
-          {/* Quote Box */}
-          <div className="lg:absolute lg:mt-[337px] lg:ml-[56px] w-full max-w-[300px] lg:w-[300px] h-auto lg:h-[142px] bg-[#1E2A38] text-white rounded-[15px] shadow-2xl z-30 mt-8 lg:mt-0 relative overflow-hidden">
-            {/* Quote Symbol */}
-            <div className="lg:absolute lg:mt-8 lg:ml-[22px] text-white text-[72px] leading-7 font-normal">
-              "
-            </div>
-            {/* Quote Text */}
-            <p className="lg:absolute lg:mt-[39px] lg:ml-[53px] text-lg lg:text-lg leading-6 font-medium max-w-[243px]">
-              Quick approval, clear process, and outstanding support throughout.
-            </p>
-          </div>
+        {/* Left Side: Use AboutDesign */}
+        <div className="order-2 lg:order-1 flex justify-center lg:block lg:min-h-[589px]">
+          <AboutDesign />
         </div>
 
         {/* Right Side: Content Column */}
